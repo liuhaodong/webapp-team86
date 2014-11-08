db1103f35112329f5677cab5119322816feef855
Haodong Liu <haodongliu@Haodongs-MBP.wv.cc.cmu.edu>
2014-11-08 15:54:45 -0500

    Modified urls.py

--- cBay/cBay/cbayweb/urls.py
+++ cBay/cBay/cbayweb/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'$','cbayweb.views.homepage',name='homepage'),
+    url(r'^homepage$','cbayweb.views.homepage',name='homepage'),
+    url(r'^viewItem$','cbayweb.views.viewItem',name='viewItem'),
 )
