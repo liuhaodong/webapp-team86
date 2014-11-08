8411c8879c29c7dab0d068a9c3fb496d6b010a6a
Sherry302 <yangweiqing001@gmail.com>
2014-11-08 15:52:48 -0500

    Modify urls.py

--- cBay/cBay/cbayweb/urls.py
+++ cBay/cBay/cbayweb/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'$','cbayweb.views.homepage',name='homepage'),
+    url(r'^$','cbayweb.views.homepage',name='homepage'),
+    url(r'^revieworder$','cbayweb.views.reviewOrder',name='reviewOrder'),
 )
